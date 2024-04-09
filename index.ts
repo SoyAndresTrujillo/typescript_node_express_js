c65cef16bd616dbfd9e9d559fbfa607e3674065a
soyandrestrujillo <andymania.1301@gmail.com>
2024-04-09 07:56:15 -0500

    benchmarking

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
-const { Transform } = require("stream");
-const { createReadStream } = require("fs");
+console.time('all')
 
-const upperCaseTransform = new Transform({
-  transform(chunk: any, encoding: any, callback: any) {
-    this.push(chunk.toString().toUpperCase());
-    callback();
-  }
-});
+console.time('sum')
+let sum: number = 0;
 
-const readableStream = createReadStream(__dirname + "/file.txt");
+for (let index = 0; index < 10000000; index++) {
+  sum++
+}
+console.timeEnd('sum')
 
-readableStream.pipe(upperCaseTransform).pipe(process.stdout);
+console.time('async')
+test()
+console.timeEnd('async')
+
+console.timeEnd('all')
+
+function test(): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log('promise')
+      resolve();
+    }, 10000)
+  })
+}
